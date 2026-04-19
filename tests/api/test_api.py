dcc82a5a9bf572d6843ce0bec86ee63c77a01aef
lrostagno <leandrorostagno7@gmail.com>
2026-04-19 10:30:40 -0300

    Mock app startup so it can run in the CI

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,18 +1,20 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 from fastapi.testclient import TestClient
 from challenge import app
 
 
 class TestBatchPipeline(unittest.TestCase):
+    
     def setUp(self):
-        self.client = TestClient(app)
-        
-        app.state.model = MagicMock()
-        app.state.model.predict.return_value = [0]
-        with TestClient(app) as client:
-            self.client = client
+        # Patch the startup function so it does nothing
+        with patch("challenge.api.startup", return_value=None):
+            self.client = TestClient(app)
+            
+            # Manually inject the mock model since startup was skipped
+            app.state.model = MagicMock()
+            app.state.model.predict.return_value = [0]
         
     def test_should_get_predict(self):
         data = {
